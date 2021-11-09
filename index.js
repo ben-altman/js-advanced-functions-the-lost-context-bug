c90ebdea14003059d5f935c58341fe21cc033b86
github username <github email address>
2021-11-08 21:56:38 -0500

    commit tree b7ef8935bddd267f053a13fe39c5d9f19045b473

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let configuration = {
   ]
 }
 
+//Solution 3: Use an Arrow Function Expression to create a function without its own context.
 let printCard = function() {
   console.log(this.frontContent)
   console.log(this.insideContent)
@@ -20,5 +21,33 @@ let printCard = function() {
   this.signatories.forEach(s => console.log(`${this.closing[s]}, ${s}`)
   )
 }
+
+//Solution 1: using thisArg with forEach: 
+/*
+let printCard = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+
+  this.signatories.forEach(function(signatory){
+      let message = `${this.closing[signatory]}, ${signatory}`
+      console.log(message)
+  }, this) //In the call to forEach, we tell it to use (for its own context) the context that printCard has as printCard's this.
+}
+*/
+
+//Solution 2. use a Closure to regain access to lost context
+/*
+let printCard = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+
+  let outerContext = this
+
+  this.signatories.forEach(function(signatory){
+      let message = `${outerContext.closing[signatory]}, ${signatory}`
+      console.log(message)
+  })
+}
+*/
 
 printCard.call(configuration)
